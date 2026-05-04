68fb3cb4c9d9f297aa6c5693eaac16eec48b05fd
profissionale928-collab <profissionale928@gmail.com>
2026-05-04 05:15:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ async function handleSearch(e) {
             'founded.gte': dataInicioISO,
             'founded.lte': dataFimISO,
             'company.simei.optant.eq': 'true', // Filtro MEI reativado
-            'limit': '200' // Aumentado o limite para buscar mais resultados
+            'limit': '100' // Aumentado o limite para buscar mais resultados
         });
 
         const url = `${API_BASE_URL}?${params.toString()}`;
